8b45cedc3e242216f7c8f04d432d80e8e6199dee
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2024-11-08 13:50:06 +0530

    commit

--- src/classifier.py
+++ src/classifier.py
@@ -205,8 +205,8 @@ class Classifier(object):
         entropy = self._valid_mean(entropy, valid_pixels, (1, 2, 3))
         marginal = self._valid_mean(probas, valid_pixels.unsqueeze(2), (1, 3, 4))
 
-        d_kl = (marginal * torch.log(1e-10 + marginal / (self.pi + 1e-10))).sum(1)
-        # d_kl = ((1-(torch.pow(marginal, self.alpha_values)*torch.pow(self.pi + 1e-10, 1-self.alpha_values)).sum(1))/(self.alpha_values-1))
+        # d_kl = (marginal * torch.log(1e-10 + marginal / (self.pi + 1e-10))).sum(1)
+        d_kl = ((1-(torch.pow(marginal, self.alpha_values)*torch.pow(self.pi + 1e-10, 1-self.alpha_values)).sum(1))/(self.alpha_values-1))
 
         if reduction == 'sum':
             entropy = entropy.sum(0)
